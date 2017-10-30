fe31ce60c7859b24e296b5877d5e8452ebd43e8f
Eduardo Edson Batista Cordeiro Alves <edu-23@hotmail.com>
2017-10-30 13:47:57 -0200

    instruções berg

--- django_project/usuarios/forms.py
+++ django_project/usuarios/forms.py
@@ -14,85 +14,85 @@ from .models import Atendente, Coordenador, FisioterapiaBerg
 
 class FisioterapiaBergForm(ModelForm):
     berg_1 = forms.ChoiceField(
-        label=_('1. Posição sentada para posição em pé: '),
+        label=_('1. Posição sentada para posição em pé:<br />Instruções: Por favor, levante-se. Tente não usar suas mãos para se apoiar.'),
         required=True,
         widget=forms.RadioSelect(attrs={'class': 'Radio'}),
         choices=ESCALA_FUNCIONAL_BERG[1])
 
     berg_2 = forms.ChoiceField(
-        label=_('2. Permanecer em pé sem apoio: '),
+        label=_('2. Permanecer em pé sem apoio:<br />Instruções: Por favor, fique em pé por 2 minutos sem se apoiar.'),
         required=True,
         widget=forms.RadioSelect(attrs={'class': 'Radio'}),
         choices=ESCALA_FUNCIONAL_BERG[2])
 
     berg_3 = forms.ChoiceField(
-        label=_('3. Permanecer sentado sem apoio nas costas, mas com os pés apoiados no chão ou num banquinho: '),
+        label=_('3. Permanecer sentado sem apoio nas costas, mas com os pés apoiados no chão ou num banquinho:<br />Instruções: Por favor, fique sentado sem apoiar as costas com os braços cruzados por 2 minutos.'),
         required=True,
         widget=forms.RadioSelect(attrs={'class': 'Radio'}),
         choices=ESCALA_FUNCIONAL_BERG[3])
 
     berg_4 = forms.ChoiceField(
-        label=_('4. Posição em pé para posição sentada: '),
+        label=_('4. Posição em pé para posição sentada:<br />Instruções: Por favor, sente-se.'),
         required=True,
         widget=forms.RadioSelect(attrs={'class': 'Radio'}),
         choices=ESCALA_FUNCIONAL_BERG[4])
 
     berg_5 = forms.ChoiceField(
-        label=_('5. Transferências: '),
+        label=_('5. Transferências:<br />Instruções: Arrume as cadeiras perpendicularmente ou uma de frente para a outra para uma transferência em pivô. Peça ao paciente para transferir-se de uma cadeira com apoio de braço para uma cadeira sem apoio de braço, e vice-versa. Você poderá utilizar duas cadeiras (uma com e outra sem apoio de braço) ou uma cama e uma cadeira.'),
         required=True,
         widget=forms.RadioSelect(attrs={'class': 'Radio'}),
         choices=ESCALA_FUNCIONAL_BERG[5])
 
     berg_6 = forms.ChoiceField(
-        label=_('6. Permanecer em pé sem apoio com os olhos fechados: '),
+        label=_('6. Permanecer em pé sem apoio com os olhos fechados:<br />Instruções: Por favor, fique em pé e feche os olhos por 10 segundos.'),
         required=True,
         widget=forms.RadioSelect(attrs={'class': 'Radio'}),
         choices=ESCALA_FUNCIONAL_BERG[6])
 
     berg_7 = forms.ChoiceField(
-        label=_('7. Permanecer em pé sem apoio com os pés juntos: '),
+        label=_('7. Permanecer em pé sem apoio com os pés juntos:<br />Instruções: Junte seus pés e fique em pé sem se apoiar.'),
         required=True,
         widget=forms.RadioSelect(attrs={'class': 'Radio'}),
         choices=ESCALA_FUNCIONAL_BERG[7])
 
     berg_8 = forms.ChoiceField(
-        label=_('8. Alcançar a frente com o braço estendido permanecendo em pé: '),
+        label=_('8. Alcançar a frente com o braço estendido permanecendo em pé:<br />Instruções: Levante o braço a 90º. Estique os dedos e tente alcançar a frente o mais longe possível. (O examinador posiciona a régua no fim da ponta dos dedos quando o braço estiver a 90º. Ao serem esticados para frente, os dedos não devem tocar a régua. A medida a ser registrada é a distância que os dedos conseguem alcançar quando o paciente se inclina para frente o máximo que ele consegue. Quando possível, peça ao paciente para usar ambos os braços para evitar rotação do tronco).'),
         required=True,
         widget=forms.RadioSelect(attrs={'class': 'Radio'}),
         choices=ESCALA_FUNCIONAL_BERG[8])
 
     berg_9 = forms.ChoiceField(
-        label=_('9. Pegar um objeto do chão a partir de uma posição em pé: '),
+        label=_('9. Pegar um objeto do chão a partir de uma posição em pé:<br />Instruções: Pegue o sapato/chinelo que está na frente dos seus pés.'),
         required=True,
         widget=forms.RadioSelect(attrs={'class': 'Radio'}),
         choices=ESCALA_FUNCIONAL_BERG[9])
 
     berg_10 = forms.ChoiceField(
-        label=_('10. Virar-se e olhar para trás por cima dos ombros direito e esquerdo enquanto permanece em pé: '),
+        label=_('10. Virar-se e olhar para trás por cima dos ombros direito e esquerdo enquanto permanece em pé:<br />Instruções: Vire-se para olhar diretamente atrás de você por cima do seu ombro esquerdo sem tirar os pés do chão. Faça o mesmo por cima do ombro direito. (O examinador poderá pegar um objeto e posicioná-lo diretamente atrás do paciente para estimular o movimento).'),
         required=True,
         widget=forms.RadioSelect(attrs={'class': 'Radio'}),
         choices=ESCALA_FUNCIONAL_BERG[10])
 
     berg_11 = forms.ChoiceField(
-        label=_('11. Girar-se 360 graus: '),
+        label=_('11. Girar-se 360 graus:<br />Instruções: Gire-se completamente ao redor de si mesmo. Pausa. Gire-se completamente ao redor de si mesmo em sentido contrário.'),
         required=True,
         widget=forms.RadioSelect(attrs={'class': 'Radio'}),
         choices=ESCALA_FUNCIONAL_BERG[11])
 
     berg_12 = forms.ChoiceField(
-        label=_('12. Posicionar os pés alternadamente no degrau ou banquinho enquanto permanece em pé sem apoio: '),
+        label=_('12. Posicionar os pés alternadamente no degrau ou banquinho enquanto permanece em pé sem apoio:<br />Instruções: Toque cada pé alternadamente no degrau/banquinho. Continue até que cada pé tenha tocado o degrau/banquinho quatro vezes.'),
         required=True,
         widget=forms.RadioSelect(attrs={'class': 'Radio'}),
         choices=ESCALA_FUNCIONAL_BERG[12])
 
     berg_13 = forms.ChoiceField(
-        label=_('13. Permanecer em pé sem apoio com um pé à frente: '),
+        label=_('13. Permanecer em pé sem apoio com um pé à frente:<br />Instruções: (demonstre para o paciente) Coloque um pé diretamente à frente do outro na mesma linha; se você achar que não irá conseguir, coloque o pé um pouco mais à frente do outro pé e levemente para o lado.'),
         required=True,
         widget=forms.RadioSelect(attrs={'class': 'Radio'}),
         choices=ESCALA_FUNCIONAL_BERG[13])
 
     berg_14 = forms.ChoiceField(
-        label=_('14. Permanecer em pé sobre uma perna: '),
+        label=_('14. Permanecer em pé sobre uma perna:<br />Instruções: Fique em pé sobre uma perna o máximo que você puder sem se segurar.'),
         required=True,
         widget=forms.RadioSelect(attrs={'class': 'Radio'}),
         choices=ESCALA_FUNCIONAL_BERG[14])
